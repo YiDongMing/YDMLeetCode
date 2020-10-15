a75a7939b0a9955e365ce6c7c746f896ecde5724
yidongming <978325568@qq.com>
2020-10-16 07:05:38 +0800

    三数之和

---
+++ src/com/code/三数之和/Sszh.java
@@ -0,0 +1,46 @@
+package com.code.三数之和;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Sszh {
+
+    public static List<List<Integer>> threeSum(int[] nums){
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int first = 0; first < n; first++){
+            if(first > 0 && nums[first] == nums[first-1]){
+                continue;
+            }
+            int third = n - 1;
+            int target = -nums[first];
+            for(int second = first + 1; second < n; second++){
+                if(second > first + 1 && nums[second] == nums[second-1]){
+                    continue;
+                }
+                while (second < third && nums[second] + nums[third] > target){
+                    third--;
+                }
+                if(second == third){
+                    break;
+                }
+                if(nums[second] + nums[third] == target){
+                    List<Integer> list  = new ArrayList<>();
+                    list.add(nums[first]);
+                    list.add(nums[second]);
+                    list.add(nums[third]);
+                    ans.add(list);
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int [] nums = {-1,0,1,2,-1,-4};
+        List<List<Integer>> lists = threeSum(nums);
+        System.out.println(lists.toString());
+    }
+}
